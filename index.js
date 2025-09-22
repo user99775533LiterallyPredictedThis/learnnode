6cf95e53cd435391d9e68385e353fd12d60d784a
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-09-22 15:52:10 +0300

    Add math examples

--- index.js
+++ index.js
@@ -1,44 +1,22 @@
+let answer = 1 + 1;
+answer = 1 - 1;
+answer = 2 * 3;
+answer = 2 / 3;
+answer = 7 % 2;
+answer = 2 ** 3;
+console.log(answer);
+
 let num = 1;
+num = num + 1;
+num += 1;
+num -= 1;
+num *= 2;
+num /= 3;
+num %= 2;
+num ** 2;
+num++;
+num--;
+num = 1;
+num = Math.pow(3, 4);
+num = 0.1 + 0.2;
 console.log(num);
-console.log(typeof num);
-
-num = 1.2;
-console.log(num);
-console.log(typeof num);
-
-let text = 'Hello World!';
-console.log(text);
-console.log(typeof text);
-
-text = 'H'; // char
-console.log(text);
-console.log(typeof text);
-
-let bool = true;
-console.log(bool);
-console.log(typeof bool);
-
-bool = false;
-console.log(bool);
-console.log(typeof bool);
-
-let undef = undefined;
-console.log(undef);
-console.log(typeof undef);
-
-let nil = null;
-console.log(nil);
-console.log(typeof nil);
-
-let array = [1, 2, 'hello', 'world', true, [1, 2, 3]]; // array inside array -- multidimensional array
-console.log(array);
-console.log(typeof array);
-
-let obj = {
-    name: 'Kaspar',
-    age: 32,
-    hairColor: 'Blonde',
-    isMale: true,
-};
-console.log(obj);
-console.log(typeof obj);
