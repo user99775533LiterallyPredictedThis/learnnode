5dd522739b606dcb583ee66bb1a7f2b9aff0db8e
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-09-25 17:03:35 +0300

    Add loop examples

--- index.js
+++ index.js
@@ -1,63 +1,29 @@
-let num = 21;
-if (num > 10) {
-    console.log('Bigger than 10');
-} else if (num === 10) {
-    console.log('Equal to 10');
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+    sum += i;
 }
-else {
-    console.log('Lesser than 10');
-}
+//console.log(sum);
 
-let day = new Date("2025-09-25").getDay();
-console.log(day); // Will show the day of the week with a number (eg. Thu = 4)
-
-if (day === 1) {
-    console.log('Esmaspäev');
-} else if (day === 2) {
-    console.log('Teisipäev');
-} else if (day === 3) {
-    console.log('Kolmapäev');
-} else if (day === 4) {
-    console.log('Neljapäev');
-} else if (day === 5 || day === 6) {
-    console.log('Pidupäev');
-} else if (day === 0) {
-    console.log('Pühapäev');
-} else {
-    console.log('Imelikpäev');
+for (let i = 10; i >= 0; i--) {
+    //console.log(i);
 }
 
-switch (day) {
-    case 1:
-        console.log('Esmaspäev');
-        break;
-    case 2:
-        console.log('Teisipäev');
-        break;
-    case 3:
-        console.log('Kolmapäev');
-        break;
-    case 4:
-        console.log('Neljapäev');
-        break;
-    case 5:
-    case 6:
-        console.log('Pidupäev');
-        break;
-    case 0:
-        console.log('Pühapäev');
-        break;
-    default:
-        console.log("Imelikpäev");
+for (let i = 1; i < 1000000; i *= 2) {
+    //console.log(i);
 }
 
-let days = ['Pühapäev', 'Esmaspäev', 'Teisipäev', 'Kolmapäev', 'Neljapäev', 'Pidupäev', 'Pidupäev'];
-console.log(days[day]);
+let time = Date.now();
+let num = 0;
+while (Date.now() < time + 1000) {
+    num++;
+}
+console.log(num);
 
-num = 1;
-answer = num > 10 ? 'Suurem' : 'Väiksem';
-console.log(answer);
+num = 10;
+while (num < 10) {
+    console.log('WHILE');
+}
 
-let value = 'asdasd'; // If value left empty = 'Midagi muud'
-answer = value ?? 'Midagi muud'; // Null-coalescing operator
-console.log(answer);
+do {
+    console.log('DO');
+} while (num < 10);
