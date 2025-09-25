d2857efdd239e946d9b22dfcbc5f1ddd2454c787
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-09-25 16:11:13 +0300

    Add if and switch examples

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-let bool = true; // Self explanatory
-//console.log(bool);
-bool = false;
-//console.log(bool);
+let num = 21;
+if (num > 10) {
+    console.log('Bigger than 10');
+} else if (num === 10) {
+    console.log('Equal to 10');
+}
+else {
+    console.log('Lesser than 10');
+}
 
-bool = true && true; // AND, only true if both statements = true
-bool = true && false; // AND, otherwise false
-bool = false && true; // AND, false
-bool = false && false; // AND, false
-//console.log(bool);
+let day = new Date("2025-09-25").getDay();
+console.log(day); // Will show the day of the week with a number (eg. Thu = 4)
 
-bool = true || false; // (AND/)OR, will = true if true is present on either side
-bool = false || false; // (AND/)OR, Only false if both sides = false
-//console.log(bool);
+if (day === 1) {
+    console.log('Esmaspäev');
+} else if (day === 2) {
+    console.log('Teisipäev');
+} else if (day === 3) {
+    console.log('Kolmapäev');
+} else if (day === 4) {
+    console.log('Neljapäev');
+} else if (day === 5 || day === 6) {
+    console.log('Pidupäev');
+} else if (day === 0) {
+    console.log('Pühapäev');
+} else {
+    console.log('Imelikpäev');
+}
 
-bool = !true; //NOT, if not true then is false
-//console.log(bool);
-bool = !false; // NOT, if not false then is true
-//console.log(bool);
-
-bool = (true && false || true) && !(false || true && false);
-// Answer: bool = true;
-//console.log(bool);
-
-bool = 10 > 5; // True
-//console.log(bool);
-bool = 10 < 5; // False
-//console.log(bool);
-bool = 10 > 10; // False
-//console.log(bool);
-bool = 10 == 10; // True
-//console.log(bool);
-bool = '10' == 10; // True, non strict will convert and check
-//console.log(bool);
-bool = '10' === 10; // False, strict will check if types match before values
-//console.log(bool);
-bool = 10 >= 10; // True, greater or equals
-//console.log(bool);
-bool = '10' != 10; // False, not equals
-//console.log(bool);
-bool = '10' !== 10; // True
-//console.log(bool);
+switch (day) {
+    case 1:
+        console.log('Esmaspäev');
+        break;
+    case 2:
+        console.log('Teisipäev');
+        break;
+    case 3:
+        console.log('Kolmapäev');
+        break;
+    case 4:
+        console.log('Neljapäev');
+        break;
+    case 5:
+    case 6:
+        console.log('Pidupäev');
+        break;
+    case 0:
+        console.log('Pühapäev');
+        break;
+    default:
+        console.log("Imelikpäev");
+}
