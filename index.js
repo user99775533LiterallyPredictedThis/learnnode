8d8b99263f737618d9c518477080f7ce3b5f38c4
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-09-25 14:51:40 +0300

    Add function examples

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
-let obj = {
-    name: 'Sigma',
-    age: 67,
-    cat: {
-        name: 'Busya',
-        age: '4 months idk',
-        gender: 'female'
+function hello() {
+    console.log('Hello World!');
+}
+
+hello();
+hello();
+hello();
+hello();
+hello();
+
+function helloName(name) {
+    console.log(`Hello ${name}!`);
+}
+
+helloName('Kaspar');
+helloName('Martin');
+
+function hi(name='Stupid', age='Insert the argument') {
+    console.log(`Hi ${name} you are ${age} years old`)
+}
+
+hi('Sigma', 67);
+hi();
+
+let sum = function (a, b){
+    return a+b;
+}
+
+let answer = sum(4, 5);
+console.log(answer);
+console.log(sum(6, 10));
+sum(5, 6);
+
+let sub = (a, b) => {
+    return a-b;
+}
+
+console.log(sub(6, 10));
+
+let mult = (a, b) => a*b;
+
+console.log(mult(6, 10));
+
+let root = a => a*a;
+
+console.log(root(6, 10));
+
+let cool = () => console.log('cool');
+
+cool();
+
+let awesome = {
+    party: function () {
+        console.log('PARTY!');
     },
-    fruits: ['apple', 'banana', 'orange']
+    cool: () => console.log('cool'),
+    nice() {
+        console.log('nice!');
+    }
 };
 
-// parent-name: 'asd', Not allowed because -, unless ' ' is used
-// 2asd: 'asd', Not allowed to start with number, unless ' ' is used
-// Neither recommended, but if you will: console.log(obj['parent-name']);
-// Just don't
+// ^ Basically different ways to do the same thing or something idk....
 
-//console.log(obj)
-//console.log(obj.cat)
-//console.log(obj.fruits)
-//console.log(obj.fruits[2])
-//console.log(Object.keys(obj));
+awesome.party();
+awesome.cool();
+awesome.nice(); 
