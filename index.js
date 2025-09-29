c93abfc7466cb5c9352465498c292b6ba3fbe231
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-09-29 14:41:35 +0300

    Add recursive function example

--- index.js
+++ index.js
@@ -17,13 +17,22 @@ let num = 0;
 while (Date.now() < time + 1000) {
     num++;
 }
-console.log(num);
+//console.log(num);
 
 num = 10;
 while (num < 10) {
-    console.log('WHILE');
+    //console.log('WHILE');
 }
 
 do {
-    console.log('DO');
+    //console.log('DO');
 } while (num < 10);
+
+function recursion(num) {
+    if(num<10) {
+        console.log(num);
+        recursion(num+1);
+    }
+}
+
+recursion(0);
