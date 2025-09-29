5b1fa255c2988c7fe3a54077e7cd789f4fcdc466
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-09-29 15:40:28 +0300

    Add code that only works in browser

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-let text = 'Hello World';
-console.log(text[3]);
-let array= [1, 2, 3];
-let num = 2;
-num.toExponential();
-array.fill(1,3,6);
-console.log(text.length);
-console.log(Math.PI);
+console.log('Hello World');
+console.log(document, window);
