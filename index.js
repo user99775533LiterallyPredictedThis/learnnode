77e253f121c53e290993011a3fdb7ccdb2f84f56
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-09-29 15:05:50 +0300

    Add object examples 2

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
-let array = ['apple', 'pear', 'cherry', 'mango'];
-
-for (fruit of array) {
-    //console.log(fruit);
-}
-
-array.forEach(fruit => {
-    //console.log(fruit);
-});
-
-let obj = {
-    name: 'Sigma',
-    age: 67,
-    color: 'purple',
-}
-
-for (key in obj) {
-    //console.log(key);
-    //console.log(obj[key]);
-}
-
-let capital = array.map(fruit => {
-    return fruit.toUpperCase();
-});
-
-//console.log(capital);
-
-let aFruits = array.filter(fruit => {
-    return fruit.includes('a');
-});
-
-//console.log(aFruits);
-
-let nums = [1, 2, 3, 4, 5, 6, 7];
-
-let sum = nums.reduce((total, num) => {
-return total + num;
-});
-console.log(sum);
+let text = 'Hello World';
+console.log(text[3]);
+let array= [1, 2, 3];
+let num = 2;
+num.toExponential();
+array.fill(1,3,6);
+console.log(text.length);
+console.log(Math.PI);
