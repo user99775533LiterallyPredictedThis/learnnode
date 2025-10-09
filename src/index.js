82ea17e9ca86bdda9517ef895512918aeb6a3f3d
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-10-09 15:03:57 +0300

    Reset project

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './style.scss';
-
-
-console.log('Hello World');
-import module from "./module.js";
-
-module.hello();
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,12 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-let response = await fetch('https://rickandmortyapi.com/api/character');
-let data = await response.json();
-console.log(data);
-
-let pages = [];
-for (let character of data.results) {
-    let page = new HtmlWebpackPlugin({
-        filename: `${character.id}.html`,
-        template: './src/views/page.njk',
-        templateParameters: {
-            character, // same as character: character
-        }
-    });
-    pages.push(page);
-}
-
 export default {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(import.meta.dirname, 'dist'),
+        clean: true,
     },
     devServer: {
         static: {
@@ -51,33 +36,12 @@ export default {
                         }
                     }
                 ],
-            },
-            {
-                test: /\.njk$/,
-                use: [
-                    {
-                        loader: 'simple-nunjucks-loader',
-                        options: {}
-                    },
-                ],
-            },
+            }
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/views/index.njk',
-            templateParameters: {
-                characters: data.results
-            }
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'about.html',
-            template: './src/views/about.njk'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'contacts.html',
-            template: './src/views/contacts.njk'
-        }),
-        ...pages
+            template: "./src/index.html"
+        })
     ],
 };
