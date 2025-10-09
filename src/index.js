ffc611748b9204d644530e21aad7ba55439459a2
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-10-09 15:58:01 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.scss';
 
 let input = document.querySelector('input#new-item');
-console.log(input);
 let button = document.querySelector('#add-btn');
 let list = document.querySelector('#shopping-list');
 button.addEventListener('click', () => {
