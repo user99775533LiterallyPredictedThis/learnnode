149ca64c491890147a1e653190379c7f0c4a0899
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-10-13 15:21:59 +0300

    Add more complex item list

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,77 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from "vue";
 
-let newItem = ref('');
-let items = ref(['leib', 'sai', 'õlu']);
+let i = 1;
+let newItem = ref("");
+let items = ref([
+  { id: i++, name: "leib", isDone: true},
+  { id: i++, name: "sai", isDone: false},
+  { id: i++, name: "õlu", isDone: true},
+]);
 let code = ref(404);
-function addItem(){
-    items.value.push(newItem.value);
-    newItem.value = '';
+function addItem() {
+  items.value.push({id: i++, name: newItem.value, isDone: false});
+  newItem.value = "";
 }
+
+let doneItems = computed(() => {
+return items.value.filter(item => item.isDone);
+});
+
+let toDoItems = computed(() => {
+return items.value.filter(item => !item.isDone);
+});
 
 </script>
 <template>
   <div class="container">
     <div class="content">
-        <div class="field has-addons">
-            <div class="control is-expanded">
-                <input v-model="newItem" class="input" type="text" placeholder="Add item">
-            </div>
-            <div class="control">
-                <button class="button is-info" @click="addItem">
-                    Add item
-                </button>
-            </div>
+      <div class="field has-addons">
+        <div class="control is-expanded">
+          <input
+            v-model="newItem"
+            class="input"
+            type="text"
+            placeholder="Add item"
+          />
         </div>
-        <h1>{{ newItem }}</h1>
-        <ul>
-            <li v-for="item in items">{{ item }}</li>
-        </ul>
-        <input v-model="code" class="input" type="number" placeholder="Enter Error code">
-        <img :src="'https://http.cat/'+ code">
+        <div class="control">
+          <button class="button is-info" @click="addItem">Add item</button>
+        </div>
+      </div>
+      <h1>{{ newItem }}</h1>
+
+      <h1>All Items</h1>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+            <input type="checkbox" v-model="item.isDone"></input>
+            {{ item.name }}
+        </li>
+      </ul>
+
+      <h1>Done Items</h1>
+      <ul>
+        <li v-for="item in doneItems" :key="item.id">
+            <input type="checkbox" v-model="item.isDone"></input>
+            {{ item.name }}
+        </li>
+      </ul>
+
+      <h1>ToDo Items</h1>
+      <ul>
+        <li v-for="item in toDoItems" :key="item.id">
+            <input type="checkbox" v-model="item.isDone"></input>
+            {{ item.name }}
+        </li>
+      </ul>
+
+      <input
+        v-model="code"
+        class="input"
+        type="number"
+        placeholder="Enter Error code"
+      />
+      <img :src="'https://http.cat/' + code" />
     </div>
-</div>
+  </div>
 </template>
