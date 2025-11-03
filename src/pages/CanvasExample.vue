2329707cfe6b5e31fe1750c1334a352cff0c384d
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-11-03 15:56:09 +0200

    Add canvas mouse follow ball color effect

--- src/pages/CanvasExample.vue
+++ src/pages/CanvasExample.vue
@@ -2,35 +2,57 @@
 import { onMounted } from "vue";
 let x = 95;
 let y = 50;
-
+let balls = [];
+let r= 255;
+let g = 0;
+let b = 0;
 
 onMounted(() => {
   let mycanvas = document.querySelector("#mycanvas");
   let ctx = mycanvas.getContext("2d");
 
   setInterval(() => {
+    balls = balls
+      .map((ball) => {
+        return {
+          x: ball.x,
+          y: ball.y,
+          radius: ball.radius - 1,
+          red: ball.red - 10,
+          green: ball.green + 20,
+          blue: ball.blue + 5,
+        };
+      })
+      .filter((ball) => ball.radius > 0);
+
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+
+    balls.push({
+      x: x,
+      y: y,
+      radius: 40,
+      red: r,
+      green: g,
+      blue: b,
+    });
+
     ctx.clearRect(0, 0, 800, 800);
-    ctx.beginPath();
-    ctx.arc(x, y, 40, 0, 2 * Math.PI);
-    ctx.fillStyle = "red";
-    ctx.fill();
+
+    balls.forEach((ball) => {
+      ctx.beginPath();
+      ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+      ctx.fillStyle = `rgb(${ball.red}, ${ball.green}, ${ball.blue})`;
+      ctx.fill();
+    });
   }, 16);
 });
 
-function keydown(event) {
+function mousemove(event) {
   console.log(event);
-  if (event.code === "KeyD") {
-    x += 5;
-  }
-  if (event.code === "KeyA") {
-    x -= 5;
-  }
-  if (event.code === "KeyS") {
-    y += 5;
-  }
-    if (event.code === "KeyW") {
-    y -= 5;
-  }
+  x = event.offsetX;
+  y = event.offsetY;
 }
 </script>
 
@@ -40,6 +62,6 @@ function keydown(event) {
     tabindex="1"
     width="800"
     height="800"
-    @keydown="keydown"
+    @mousemove="mousemove"
   ></canvas>
 </template>
