77c035bf69960f46211f6a7d6e9df49d5d695b1f
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-11-20 15:37:41 +0200

    Add chalk

--- bin/terminal.js
+++ bin/terminal.js
@@ -2,18 +2,10 @@ function write(text) {
     process.stdout.write(text);
 }
 
-import readline from 'readline';
+import chalk from "chalk";
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-rl.question('What is your name?', name => {
-    console.log(`Hello ${name}!!!`);
-    rl.close();
-});
-
+write(chalk.yellow.bgRed.underline('DANGER!!!'));
+write(chalk.bgRgb(255,0,0)('COLOR'));
 
 
 
