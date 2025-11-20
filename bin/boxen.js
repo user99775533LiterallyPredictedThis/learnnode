46227e0c37b3e0469344b39ea7b95d0d2cf7626f
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-11-20 16:00:50 +0200

    Add boxen

---
+++ bin/boxen.js
@@ -0,0 +1,4 @@
+import boxen from "boxen";
+import chalk from "chalk";
+
+console.log(boxen(chalk.red('Unicorns love rainbows'), {title: 'Unicorn', titleAlignment: 'center', padding: 1, margin: 1}));
