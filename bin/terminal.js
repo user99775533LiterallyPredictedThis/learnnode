15792a05e636d30fa0b3ad435ccbd9300fcc2785
Uljana Jegorova <uljana.jegorova@tptlive.ee>
2025-11-20 15:48:51 +0200

    Add inquirer examples

--- bin/terminal.js
+++ bin/terminal.js
@@ -7,6 +7,25 @@ import chalk from "chalk";
 write(chalk.yellow.bgRed.underline('DANGER!!!'));
 write(chalk.bgRgb(255,0,0)('COLOR'));
 
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+
+    {
+        type: 'number',
+        name: 'age',
+        message: 'What is your age?'
+    },
+]);
+
+console.log(`Hello ${answers.name}!!!`);
+console.log(`You are ${answers.age} years old!!!`);
+
 
 
 
